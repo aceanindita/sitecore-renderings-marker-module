a449592e956e157f682b464e68d847e5ac66dfeb
epsilon-anindita <anindita.bhattacharya@epsilon.com>
2017-03-20 16:15:49 +0530

    Initial Commit

---
+++ Pipelines/MVC/ViewMarker/RenderViewMarkerEnd.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+using Sitecore.Mvc.Presentation;
+
+namespace RenderingsMarkerModule.Pipelines.MVC.ViewMarker
+{
+    public class RenderingsMarkerEnd : RenderRenderingProcessor
+    {
+        public override void Process(RenderRenderingArgs args)
+        {
+            if (Settings.GetBoolSetting("RenderingsMarkerModule.Enabled", false))
+            {
+                Renderer renderer = args.Rendering.Renderer;
+                if (renderer == null)
+                    return;
+
+                bool isLayout = renderer is ViewRenderer &&
+                ((ViewRenderer)renderer).Rendering.RenderingType == "Layout";
+                bool showLayoutMarkers = Settings.GetBoolSetting("RenderingsMarkerModule.ShowLayoutMarkers", false);
+
+                if (isLayout && !showLayoutMarkers) return;
+
+                args.Writer.Write("\n<!-- END: " + renderer + " -->\n");
+            }
+        }
+    }
+}
---
+++ Pipelines/MVC/ViewMarker/RenderViewMarkerStart.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+using Sitecore.Mvc.Presentation;
+
+namespace RenderingsMarkerModule.Pipelines.MVC.ViewMarker
+{
+    public class RenderingsMarkerStart : RenderRenderingProcessor
+    {
+        public override void Process(RenderRenderingArgs args)
+        {
+            if (Settings.GetBoolSetting("RenderingsMarkerModule.Enabled", false))
+            {
+                Renderer renderer = args.Rendering.Renderer;
+                if (renderer == null)
+                    return;
+
+                bool isLayout = renderer is ViewRenderer &&
+                                ((ViewRenderer) renderer).Rendering.RenderingType == "Layout";
+                bool showLayoutMarkers = Settings.GetBoolSetting("RenderingsMarkerModule.ShowLayoutMarkers", false);
+
+                if (isLayout && !showLayoutMarkers) return;
+
+                args.Writer.Write("\n<!-- START: " + renderer + " -->\n");
+            }
+        }
+    }
+}
